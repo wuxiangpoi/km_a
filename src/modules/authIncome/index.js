8e730b3d373f7e9a30cc0f27d0b9d4da9e94fdb5
wuxiang <303152175@qq.com>
2018-06-26 10:54:27 +0800

    0626

--- src/modules/authIncome/index.js
+++ src/modules/authIncome/index.js
@@ -4,6 +4,23 @@ const authIncomeController = ($scope, baseService, modalService) => {
 	$scope.displayed = [];
 	$scope.sp = {};
 	$scope.tableState = {};
+	var now = new Date();
+    $scope.sp.year = now.getFullYear();
+    $scope.sp.month = baseService.formateDay(now.getMonth());
+    $scope.dateSel = $scope.sp.year + '-' + $scope.sp.month;
+    $scope.chargeStatusOptions = [{
+            val: '',
+            name: '收费状态'
+        },
+        {
+            val: 0,
+            name: '待收费'
+        },
+        {
+            val: 1,
+            name: '已收费'
+        }
+    ];
 	$scope.callServer = function (tableState, page) {
 		if (baseService.isRealNum(page)) {
 			$scope.tableState.pagination.start = page * $scope.sp.length;
