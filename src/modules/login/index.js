a1ea10e2b8361bdf8991bc4386eb8b69bfcc5635
wuxiang <303152175@qq.com>
2018-07-09 16:57:16 +0800

    0709

--- src/modules/login/index.js
+++ src/modules/login/index.js
@@ -1,41 +1,43 @@
 const loginController = ($scope, $rootScope, baseService, userService) => {
     $scope.account = '';
-        $scope.password = '';
-        $scope.isRemembered = true;
-        $scope.isShowMessage = false;
-        if($.cookie('user_cookie') && $.cookie('user_cookie').length != 4){
-            var cookiesData = JSON.parse($.cookie('user_cookie'));
-            $scope.account = cookiesData.account;
-            $scope.password = cookiesData.password;
-        }
-        
-        $scope.login = function () {
-            if ($scope.loginForm.$valid) {
-                var postData = {
-                    account: $scope.account,
-                    password: baseService.md5_pwd($scope.password)
-                }
-                userService.login(postData, function (pdata) {
-                    userService.getUserSrc(function (userData) {
-                        $rootScope.userData = userData;
-                        baseService.goToState('dashboard.home');
-                        if ($scope.isRemembered) {
-                            $.cookie('user_cookie', JSON.stringify({
-                                domain: $scope.domain,
-                                account: $scope.account,
-                                password: $scope.password
-                            }), { expires: 30 });
-                        }else{
-                            $.cookie('user_cookie', null);
-                        }
-                        
-                    });
-                })
-            } else {
-                $scope.isShowMessage = true;
+    $scope.password = '';
+    $scope.isRemembered = true;
+    $scope.isShowMessage = false;
+    if ($.cookie('user_cookie') && $.cookie('user_cookie').length != 4) {
+        var cookiesData = JSON.parse($.cookie('user_cookie'));
+        $scope.account = cookiesData.account;
+        $scope.password = cookiesData.password;
+    }
+
+    $scope.login = function () {
+        if ($scope.loginForm.$valid) {
+            var postData = {
+                account: $scope.account,
+                password: baseService.md5_pwd($scope.password)
             }
+            userService.login(postData, function (pdata) {
+                userService.getUserSrc(function (userData) {
+                    $rootScope.userData = userData;
+                    baseService.goToState('dashboard.home');
+                    if ($scope.isRemembered) {
+                        $.cookie('user_cookie', JSON.stringify({
+                            domain: $scope.domain,
+                            account: $scope.account,
+                            password: $scope.password
+                        }), {
+                            expires: 30
+                        });
+                    } else {
+                        $.cookie('user_cookie', null);
+                    }
+
+                });
+            })
+        } else {
+            $scope.isShowMessage = true;
         }
-       
+    }
+
 }
 
 loginController.$inject = ['$scope', '$rootScope', 'baseService', 'userService'];
